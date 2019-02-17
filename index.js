18761017d8412f6cab37f4b96dd1df295df7febb
fusijie <fusijie@vip.qq.com>
2019-02-17 18:19:47 +0800

    add RNEmpty

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 
-import App from "./App";
+// import App from "./App";
 // import App from "./examples/SimpleAppMovies";
+import App from "./examples/RNEmpty";
 
 AppRegistry.registerComponent(appName, () => App);
