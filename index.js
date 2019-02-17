21abf1b8d51ea1572f34d8a366ac34368039297f
fusijie <fusijie@vip.qq.com>
2019-02-17 20:01:46 +0800

    update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import { name as appName } from "./app.json";
 // import App from "./examples/HelloWorld";
 // import App from "./examples/SimpleAppMovies";
 // import App from "./examples/RNImage";
-import App from "./examples/RNAnimated";
+// import App from "./examples/RNAnimated";
+import App from "./examples/RNLayoutAnimation";
 
 AppRegistry.registerComponent(appName, () => App);
