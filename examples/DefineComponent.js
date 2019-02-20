43f778bffdd61645da384690cb0f0d57538ae2e5
fusijie <fusijie@vip.qq.com>
2019-02-20 12:23:21 +0800

    add definedComponent

---
+++ examples/DefineComponent.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import createReactClass from "create-react-class";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
+
+/**
+ * ES6
+ */
+// class NewComponent extends Component {
+//   render() {
+//     return <Text>Hello {this.props.name}</Text>;
+//   }
+// }
+
+/**
+ * ES5
+ * 已经移除了React.createClass
+ * 需要使用 npm install create-react-class --save-dev
+ */
+let NewComponent = createReactClass({
+  render() {
+    return <Text>Hello {this.props.name}</Text>;
+  }
+});
+
+/**
+ * 函数式定义
+ */
+// function NewComponent(props) {
+//   return <Text>Hello {props.name}</Text>;
+// }
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <NewComponent name="New Component!" />
+      </View>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import { name as appName } from "./app.json";
 // import App from "./examples/RNImage";
 // import App from "./examples/RNAnimated";
 // import App from "./examples/RNLayoutAnimation";
-import App from "./examples/RNTimeout";
+// import App from "./examples/RNTimeout";
+import App from "./examples/DefineComponent";
 
 AppRegistry.registerComponent(appName, () => App);
