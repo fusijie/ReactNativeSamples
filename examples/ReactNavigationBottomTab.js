72d85c8aa644851e09ea8ca3980f53885390608e
fusijie <fusijie@vip.qq.com>
2019-02-26 00:58:27 +0800

    update

--- examples/ReactNavigationBottomTab.js
+++ examples/ReactNavigationBottomTab.js
@@ -43,6 +43,42 @@ class SettingsScreen extends Component {
   }
 }
 
+class IconWithBadge extends Component {
+  render() {
+    const { name, badgeCount, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // If you're using react-native < 0.57 overflow outside of the parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: "absolute",
+              right: -6,
+              top: -3,
+              backgroundColor: "red",
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 10, fontWeight: "bold" }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const HomeIconWithBadge = props => {
+  return <IconWithBadge {...props} badgeCount={3} />;
+};
+
 const AppNavigator = createBottomTabNavigator(
   {
     Home: HomeScreen,
@@ -56,7 +92,7 @@ const AppNavigator = createBottomTabNavigator(
         let iconName;
         if (routeName === "Home") {
           icon_name = `ios-information-circle${focused ? "" : "-outline"}`;
-          // IconComponent = HomeIconWithBadge;
+          IconComponent = HomeIconWithBadge;
         } else if (routeName === "Settings") {
           icon_name = `ios-options${focused ? "" : "-outline"}`;
         }
