00ed92c0bdd6638e6fd436f481480b67449ba4ac
fusijie <fusijie@vip.qq.com>
2019-03-21 15:37:44 +0800

    update

--- examples/Test.js
+++ examples/Test.js
@@ -1,43 +1,18 @@
 import React, { Component } from "react";
 import { ScrollView, Image, StyleSheet, Text, View } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  innerContainer: {
-    flex: 1,
-    borderWidth: 2,
-    borderColor: "red"
-  }
-});
-
 export default class App extends Component {
   render() {
     return (
       <View
         style={{
-          height: 20,
-          backgroundColor: "pink",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-          marginTop: 100
+          flex: 1,
+          backgroundColor: "pink"
+          // alignItems: "center"
         }}
       >
-        <View
-          style={{
-            width: 50,
-            height: 50,
-            backgroundColor: "red",
-            alignSelf: "flex-end"
-          }}
-        />
-        <View style={{ width: 50, height: 50, backgroundColor: "green" }} />
-        <View style={{ width: 50, height: 50, backgroundColor: "blue" }} />
+        {/* <View style={{ width: 50, backgroundColor: "blue" }} /> */}
+        <ScrollView style={{ width: 200, backgroundColor: "blue" }} />
       </View>
     );
   }
